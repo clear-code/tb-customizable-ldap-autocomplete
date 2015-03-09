93ebb422aeb299902cfdcbfc69718a65d3906016
YUKI Hiroshi <shimoda@clear-code.com>
2015-03-09 15:01:45 +0900

    Add examples of preference value

--- defaults/preferences/customizable-ldap-autocomplete.js
+++ defaults/preferences/customizable-ldap-autocomplete.js
@@ -2,4 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// you can specify two styles:
+//  1. use all LDAP servers: "*"
+//  2. use specific LDAP servers: "ldap_2.servers.key1,ldap_2.servers.key2,..."
 pref("extensions.customizable-ldap-autocomplete@clear-code.com.directoryServers", "*");
